5b32aa09a133e8f4654973b21ad41486f588d82a
Le Xuan Thang <lxthanghy@gmail.com>
2020-10-03 00:08:49 +0700

    031020201208AM

--- eShopSolution.BackendApi/Controllers/ProductController.cs
+++ eShopSolution.BackendApi/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using eShopSolution.Application.Catalog.Products;
+using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Catalog.Products.Manage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,15 +15,75 @@ namespace eShopSolution.BackendApi.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
-        public ProductController(IPublicProductService publicProductService)
+        private readonly IManageProductService _manageProductService;
+
+        public ProductController(IPublicProductService publicProductService, IManageProductService manageProductService)
         {
             _publicProductService = publicProductService;
+            _manageProductService = manageProductService;
         }
-        [HttpGet]
-        public async Task<IActionResult> Get()
+
+        //http://localhost:port/product
+        [HttpGet("{languageId}")]
+        public async Task<IActionResult> Get(string languageId)
         {
-            var products = await _publicProductService.GetAll();
+            var products = await _publicProductService.GetAll(languageId);
             return Ok(products);
+        }
+
+        //http://localhost:port/product/public-paging
+        [HttpGet("public-paging/{languageId}")]
+        public async Task<IActionResult> Get([FromQuery] GetPublicProductPagingRequest request)
+        {
+            var products = await _publicProductService.GetAllByCategoryId(request);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] ProductCreateRequest request)
+        {
+            var productId = await _manageProductService.Create(request);
+            if (productId == 0)
+                return BadRequest();
+            var product = await _manageProductService.GetById(productId, request.LanguageId);
+            return CreatedAtAction(nameof(GetById), new { id = productId }, product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromForm] ProductUpdateRequest request)
+        {
+            var affectedResult = await _manageProductService.Update(request);
+            if (affectedResult == 0)
+                return BadRequest();
+            return Accepted();
+        }
+
+        [HttpPut("price/{id}/{newPrice}")]
+        public async Task<IActionResult> UpdatePrice(int id, decimal newPrice)
+        {
+            var isSuccessful = await _manageProductService.UpdatePrice(id, newPrice);
+            if (!isSuccessful)
+                return BadRequest();
+            return Accepted();
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var affectedResult = await _manageProductService.Delete(productId);
+            if (affectedResult == 0)
+                return BadRequest();
+            return Accepted();
+        }
+
+        //http://localhost:port/product/1
+        [HttpGet("{productId}/{languageId}")]
+        private async Task<IActionResult> GetById(int productId, string languageId)
+        {
+            var product = await _manageProductService.GetById(productId, languageId);
+            if (product == null)
+                return NotFound("Cannot find product");
+            return Ok(product);
         }
     }
 }
